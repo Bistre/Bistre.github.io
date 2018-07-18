7c4b59c83ee769ccd0843bba99740ef6e6f1acd8
Bistre <39963123+Bistre@users.noreply.github.com>
2018-07-18 14:33:22 -0500

    added js

---
+++ script.js
@@ -0,0 +1,8 @@
+var clear = document.getElementById("clear")
+var header = document.getElementById("header")
+
+tog.addEventListener("click", function() {
+	
+	header.classList.toggle("m-fadeOut");
+});
+	
